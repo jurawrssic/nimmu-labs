0d22ccd1d23e320365e4afe0240e023ce2eeadf9
Julia Rodrigues <juliardgs@yahoo.com.br>
2024-03-19 02:39:32 -0300

    feat: create experience page

---
+++ app/experience/page.tsx
@@ -0,0 +1,59 @@
+const ExperienceAndSkillsPage = () => {
+  return (
+    <div className="experience-page wrapper">
+      <div className="about">
+        <div className="about__title">MY SKILLS</div>
+        <div className="about__content">
+          <ul>
+            <li>React.js</li>
+            <li>JavaScript</li>
+            <li>Typescript</li>
+            <li>Next.js</li>
+            <li>Redux</li>
+            <li>Context API</li>
+            <li>NestJS</li>
+            <li>Git</li>
+            <li>Jest</li>
+            <li>Cypress</li>
+            <li>PostgreSQL</li>
+            <li>DBeaver</li>
+            <li>Code Review</li>
+            <li>jQuery</li>
+            <li>CSS</li>
+            <li>HTML</li>
+            <li>SASS</li>
+            <li>D3.js</li>
+            <li>Mentoring</li>
+            <li>TDD</li>
+            <li>Pair Programming</li>
+            <li>Interviewing</li>
+            <li>Literature Reviews</li>
+            <li>Google Maps API</li>
+            <li>Requirements Gathering</li>
+            <li>Coordinate Meetings</li>
+            <li>Java</li>
+            <li>Firebird</li>
+            <li>Microsoft Office</li>
+            <li>Hardware</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="about">
+        <div className="about__title">WORK EXPERIENCE</div>
+        <div className="about__content">
+          <p></p>
+        </div>
+      </div>
+
+      <div className="about">
+        <div className="about__title">EDUCATION</div>
+        <div className="about__content">
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExperienceAndSkillsPage;
