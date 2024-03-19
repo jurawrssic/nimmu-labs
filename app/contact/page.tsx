986c66612bd0b9c474511a19ad5a10fd71341504
Julia Rodrigues <juliardgs@yahoo.com.br>
2024-03-19 04:42:40 -0300

    feat: populate and style contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,14 @@
 const ContactPage = () => {
   return (
-    <div className="contact-page wrapper">
-      <div className="about">
-        <div className="about__title">CONTACT ME</div>
-        <div className="about__content">
-          <p></p>
-        </div>
+    <div className="contact-page about">
+      <div className="about__title">CONTACT ME</div>
+      <div className="about__content">
+        <input id="sender-email" placeholder="Email" />
+        <input id="email-subject" placeholder="Subject" />
+
+        <textarea id="email-content" placeholder="Enter your message here" />
+
+        <button>Send Message</button>
       </div>
     </div>
   );
