a38c0301c00982a2245894358a6719a41641b2c0
Julia Rodrigues <juliardgs@yahoo.com.br>
2024-03-19 02:39:22 -0300

    feat: create contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+const ContactPage = () => {
+  return (
+    <div className="contact-page wrapper">
+      <div className="about">
+        <div className="about__title">CONTACT ME</div>
+        <div className="about__content">
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactPage;
