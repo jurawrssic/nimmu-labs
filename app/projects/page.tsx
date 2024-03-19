ab7b0cadc9095684371c6ad1e30f0233cba545e4
Julia Rodrigues <juliardgs@yahoo.com.br>
2024-03-19 02:39:43 -0300

    feat: create projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,14 @@
+const ProjectsPage = () => {
+  return (
+    <div className="projects-page wrapper">
+      <div className="about">
+        <div className="about__title">SIDE-PROJECTS</div>
+        <div className="about__content">
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsPage;
