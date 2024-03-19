993a9c8eea925ebf7f15b75c06b8d124f2dc2af6
Julia Rodrigues <juliardgs@yahoo.com.br>
2024-03-19 04:42:50 -0300

    feat: add disclaimer to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ const Homepage = () => {
   return (
     <div className="home">
       <div className="home__description">
+        <p>[DISCLAIMER] This portfolio is still under progress.</p>
         <h1>JULIA RODRIGUES</h1>
         <p>Front-end Developer</p>
       </div>
