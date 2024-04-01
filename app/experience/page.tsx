49bfae36f577c915e12785c4488ddfd7db11931c
Julia Rodrigues <juliardgs@yahoo.com.br>
2024-04-01 19:07:07 -0300

    feat: add a other skills section to experience page

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -6,35 +6,41 @@ const ExperienceAndSkillsPage = () => {
         <div className="about__content">
           <ul>
             <li>React.js</li>
+            <li>Next.js</li>
             <li>JavaScript</li>
             <li>Typescript</li>
-            <li>Next.js</li>
             <li>Redux</li>
             <li>Context API</li>
-            <li>NestJS</li>
-            <li>Git</li>
-            <li>Jest</li>
             <li>Cypress</li>
+            <li>Jest</li>
+            <li>Git</li>
+            <li>CSS</li>
+            <li>SASS</li>
+            <li>HTML</li>
+            <li>jQuery</li>
+            <li>D3.js</li>
+            <li>Google Maps API</li>
+            <li>NestJS</li>
             <li>PostgreSQL</li>
             <li>DBeaver</li>
-            <li>Code Review</li>
-            <li>jQuery</li>
-            <li>CSS</li>
-            <li>HTML</li>
-            <li>SASS</li>
-            <li>D3.js</li>
-            <li>Mentoring</li>
+            <li>Firebird</li>
+            <li>Java</li>
             <li>TDD</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="about skills">
+        <div className="about__title">OTHER SKILLS</div>
+        <div className="about__content">
+          <ul>
+            <li>Code Review</li>
+            <li>Mentoring</li>
             <li>Pair Programming</li>
             <li>Interviewing</li>
             <li>Literature Reviews</li>
-            <li>Google Maps API</li>
             <li>Requirements Gathering</li>
             <li>Coordinate Meetings</li>
-            <li>Java</li>
-            <li>Firebird</li>
-            <li>Microsoft Office</li>
-            <li>Hardware</li>
           </ul>
         </div>
       </div>
